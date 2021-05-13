89b946f5bb60a838b66206773dee4f3f9f8b8194
= <=>
2021-05-13 00:03:29 -0300

    ajuste nos materiais de aula

--- material_apoio/Utilização de operadores.sql
+++ material_apoio/Utilização de operadores.sql
@@ -1,47 +1,78 @@
-# comando para pesquisar dados	
+-----------------------------------------------------------
+# comando para pesquisar dados
+-----------------------------------------------------------
 SELECT * FROM gravadora;
 
-#Operadores aritmeticos +,-,*,/
+# Operadores aritmeticos +,-,*,/
 SELECT 1 + 1;
 SELECT 10 - 5;
 SELECT 10 * 10;
 SELECT 100 / 10;
 
+-----------------------------------------------------------
 # Operadores relacionais <, > <=, >= != <>, =
+-----------------------------------------------------------
 SELECT codigo_gravadora
      , nome_gravadora
 	 , contato_gravadora
-	 , endereco_gravadora 
+	 , endereco_gravadora
   FROM gravadora
- WHERE codigo_gravadora <> 2;	
- 
+ WHERE codigo_gravadora <> 2;
+
+----------------------------------------------------------- 
 # Operadores logicos AND, OR, NOT
+-----------------------------------------------------------
 SELECT *
   FROM gravadora
  WHERE codigo_gravadora > 0
-   OR (nome_gravadora = 'EMI' 
-	     OR contato_gravadora = 'joãoo' 
-		 AND url_gravadora = 'www.emi-music.com.br');  
-  
- # operadores de conjunto IN, NOT IN - BETWEEN
- SELECT * 
-  FROM gravadora
- WHERE codigo_gravadora IN (1, 4, 3, 2, 1000, 5000); 
- 
- SELECT * 
-  FROM gravadora
- WHERE codigo_gravadora NOT IN (1, 2, 1000, 5000); 
- 
-  SELECT * 
-  FROM gravadora
- WHERE codigo_gravadora  BETWEEN 1 AND 200; 
-
-# NULL
+   AND  nome_gravadora = 'EMI'
+   AND  contato_gravadora = 'joão'
+    OR url_gravadora = 'www.emi-music.com.br');
+-----------------------------------------------------------
+# Operadores de conjunto IN, NOT IN - BETWEEN, NOT BETWEEN
+-----------------------------------------------------------
 SELECT *
   FROM gravadora
- WHERE gravadora.endereco_Gravadora is not null;
+ WHERE codigo_gravadora IN (1, 4, 3, 2, 1000, 5000);
+ 
+SELECT *
+  FROM gravadora
+ WHERE codigo_gravadora NOT IN (1, 2, 1000, 5000);
+ 
+SELECT *
+  FROM gravadora
+ WHERE codigo_gravadora  BETWEEN 1 AND 200;
 
-# Operador LIKE 
- SELECT *
-   FROM gravadora
-  WHERE gravadora.Nome_Gravadora LIKE '%ale%sandro%';	  
+SELECT *
+  FROM gravadora
+ WHERE codigo_gravadora  NOT BETWEEN 2 AND 4;
+
+-----------------------------------------------------------
+# Operadores NULL
+-----------------------------------------------------------
+SELECT *
+  FROM gravadora
+ WHERE gravadora.endereco_Gravadora IS NULL;
+
+SELECT *
+  FROM gravadora
+ WHERE gravadora.endereco_Gravadora IS NOT NULL;
+ 
+-----------------------------------------------------------
+# Operador LIKE em conjunto com a fução UPPER
+-----------------------------------------------------------
+SELECT *
+  FROM gravadora
+ WHERE gravadora.Nome_Gravadora LIKE 'EMI%';
+ 
+SELECT *
+  FROM gravadora
+ WHERE gravadora.Nome_Gravadora LIKE '%EMI';
+ 
+SELECT *
+  FROM gravadora
+ WHERE gravadora.Nome_Gravadora LIKE '%EMI%';
+ 
+SELECT *
+  FROM gravadora
+ WHERE gravadora.Nome_Gravadora LIKE '%EMI%EMI%'; 
